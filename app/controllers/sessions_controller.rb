c0c4ecf0c0b4b80f441ec995ffd810586fcad88d
Nathan Venn <natevenn@gmail.com>
2016-02-27 11:07:18 -0700

    7 add authentication to user in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    if @user
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else

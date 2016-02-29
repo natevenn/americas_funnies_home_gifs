bdaae13123b00fded67c0c01705aad0924b93da8
Nathan Venn <natevenn@gmail.com>
2016-02-29 16:33:42 -0700

    add read me file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   validates :name, presence: :true, uniqueness: :true
 
-  has_many :gifs
+  has_many :gifs, dependent: :destroy
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :favorites, dependent: :destroy
-  has_many :gifs, through: :favorites, dependent: :destroy
+  has_many :favorites
+  has_many :gifs, through: :favorites
 
   enum role:  %w(default admin)
 end
